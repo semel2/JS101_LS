2a3809dcd06f689ef94593ef4921d1c2164aa21e
semel2 <benjaminsemel@gmail.com>
2021-09-21 07:18:59 -0500

    pushing deletions

--- lesson_6/debugTester.js
+++
@@ -1,7 +0,0 @@
-let array = [1,2,3,4,5,6];
-
-array[2] = 'j';
-
-for (let i = 0; i < array.length; i += 1) {
-  console.log(array[i]);
-}
--- lesson_6/test.js
+++
@@ -1,5 +0,0 @@
-const readline = require("readline-sync");
-
-let leftArrow = readline.question('What does left arrow look like?')
-
-console.log(leftArrow);
